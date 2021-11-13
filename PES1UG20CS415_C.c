b46bb77224b6b2e795c4f5a4f4b53471a4942720
shrujandev <shrujandev1824@gmail.com>
2021-11-13 12:01:07 +0530

    Initial Commit

--- PES1UG20CS415_C.c
+++ PES1UG20CS415_C.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
-#include<conio.h>
+#include<stdlib.h>
 
 
 int main(){
+    int a[100];
     FILE *input = fopen("input.txt","r");
     FILE *output = fopen("output.txt","w");
+    int i=0;
+    if(input == NULL){
+        printf("Ohh error\n");
+        return 0;
+    }
 
-    
+
+    while ( fscanf(input, "%d", &a[i] ) != EOF ){
+        i++;
+    };
+
+    for(int i = a[0];i<a[3];i++){
+        for(int j = a[0];j<a[3];j++){
+            printf(" %d ",a[j+4]);
+
+        
+        } 
+        printf("\n");
+    } 
+
+    return 0;
 
 }
