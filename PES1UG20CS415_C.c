9a0754f339c011419d34fcaa2f1972243dffd1bc
shrujandev <shrujandev1824@gmail.com>
2021-11-13 23:04:07 +0530

    Read write from file

--- PES1UG20CS415_C.c
+++ PES1UG20CS415_C.c
@@ -3,7 +3,10 @@
 
 
 int main(){
-    int a[100];
+    int a;
+    int b[500];
+    int c[100][100];
+    int startI,startJ,endI,endJ;
     FILE *input = fopen("input.txt","r");
     FILE *output = fopen("output.txt","w");
     int i=0;
@@ -11,20 +14,44 @@ int main(){
         printf("Ohh error\n");
         return 0;
     }
+    int j=0;
+   
+    while ( fscanf(input, "%d", &a ) == 1 ){
+        switch(j){
+            case 0 : startI = a;
+                break;
+            case 1 : startJ = a;
+                break;
+            case 2: endI =a;
+                break;
+            case 3: endJ = a;
+                break;
+            default :
+                    b[j-4] = a;
+        }
+        j++;
 
-
-    while ( fscanf(input, "%d", &a[i] ) != EOF ){
-        i++;
     };
+   a = 0;
+    for(int i = startI; i <= endI ;i++){
+        for(int j =startJ ; j <= endJ ; j++ ){
+            c[i][j] = b[a];
+            a++;
+        }
+    }
 
-    for(int i = a[0];i<a[3];i++){
-        for(int j = a[0];j<a[3];j++){
-            printf(" %d ",a[j+4]);
+    for(int i = startI; i <= endI ;i++){
+        for(int j =startJ ; j <= endJ ; j++ ){
+            printf(" %d ",c[i][j]);
 
-        
-        } 
+        }
         printf("\n");
-    } 
+    }
+
+    
+    fclose(input);
+    fclose(output);
+    
 
     return 0;
 
