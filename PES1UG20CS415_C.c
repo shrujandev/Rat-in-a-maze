c5214f7acd155f1f028c3836e7ca70e6bb977306
shrujandev <shrujandev1824@gmail.com>
2021-11-16 16:56:22 +0530

    Works fine with some slight error

--- PES1UG20CS415_C.c
+++ PES1UG20CS415_C.c
@@ -14,9 +14,23 @@ typedef struct head{
     struct node *head;
 }Head;
 
+typedef struct stackele{
+    struct node *ele;
+    struct stackele *next;
+}StackEle;
+
+typedef struct stack{
+    struct stackele *head;
+}Stack;
+
 void init(Head *ptr){
    ptr->head = NULL;
 }
+
+Node* popElement(Stack *stack);
+void inputMatrix(Head *ptr,int c,int i, int j);
+void findPath(Head *ptr,int endI,int endJ);
+void pushElement(Stack *stack,Node *element);
 
 int main(){
     int a;
@@ -69,16 +83,7 @@ int main(){
 
 
 
-Node *p = ptr->head;
-printf("%d",p->data);
-p=p->right;
-printf("%d",p->data);
-p=p->down;
-printf("%d",p->data);
-p=p->right;
-printf("%d",p->data);
-p=p->down;
-printf("%d",p->data);
+    findPath(ptr,endI,endJ);
 
     
     fclose(input);
@@ -157,4 +162,75 @@ void inputMatrix(Head *ptr,int c,int i, int j){
         
     }
     printf("done");
+}
+
+
+void findPath(Head *ptr,int endI,int endJ){
+            Stack *stack;
+            stack = (Stack*)malloc(sizeof(Stack));
+            stack->head = NULL;
+            Node *move;
+            move = ptr->head;
+            pushElement(stack,move);
+            while(move->i != endI && move->j != endJ){
+                if(move->right->data == 0 ){
+                    
+                    move = move->right;
+                    pushElement(stack,move);
+                    
+                }
+                else if(move->down->data ==0){
+                    
+                    move = move->down;
+                    pushElement(stack,move);
+                    
+
+                }
+                else if(move->down->data !=0 && move->right->data !=0){
+                    move->data = 1;
+                    move = popElement(stack);
+                }
+            }
+
+            StackEle *p;
+            p = stack->head;
+            printf("\nThe result");
+            while(p->next != NULL){
+                printf("\n%d %d",p->ele->i,p->ele->j);
+                p = p->next;
+
+            }
+
+}
+
+void pushElement(Stack *stack,Node *element){
+        StackEle *p = stack->head;
+        StackEle *temp;
+        
+        temp = (StackEle*)malloc(sizeof(StackEle));
+        temp->ele = element;
+        temp->next = NULL;
+        if(p == NULL){
+            stack->head =temp;
+        }
+        else{
+        while(p->next !=NULL){
+            p = p->next;
+        }
+        p->next = temp;
+        }
+}
+
+Node* popElement(Stack *stack){
+    StackEle *p = stack->head;
+    StackEle *prev;
+    while(p->next !=NULL){
+            prev = p;
+            p = p->next;
+    }
+    p->ele->data = 1;
+    prev->next = NULL;
+    
+    return prev->ele;
+
 }
